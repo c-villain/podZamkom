cc5bc512f4d02e9d15e050daefdfd91b55ebbe1b
Alexander Kraev <c-villain@mail.ru>
2013-11-09 21:56:28 +0400

    9 ноября 2013

---
+++ podZamkom/NewBankAccountVC.h
@@ -0,0 +1,14 @@
+//
+//  NewBankAccountVC.h
+//  podZamkom
+//
+//  Created by Alexander Kraev on 21.10.13.
+//  Copyright (c) 2013 Alexander Kraev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewAppearance.h"
+
+@interface NewBankAccountVC : UIViewController
+
+@end
