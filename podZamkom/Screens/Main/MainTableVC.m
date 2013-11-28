61cfb00f463adef4e92b934ef512a8714294484a
Alexander Kraev <c-villain@mail.ru>
2013-11-29 03:18:02 +0400

    йцук

--- podZamkom/Screens/Main/MainTableVC.m
+++ podZamkom/Screens/Main/MainTableVC.m
@@ -26,19 +26,13 @@
     [self.tableView reloadData];
 }
 
+
 //чтобы не было смещения в таблице относительно навбара
-- (void) viewDidLayoutSubviews {
-    // only works for iOS 7+
-    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0) {
-        CGRect viewBounds = self.view.bounds;
-        CGFloat topBarOffset = self.topLayoutGuide.length;
-        
-        // snaps the view under the status bar (iOS 6 style)
-        viewBounds.origin.y = topBarOffset * -1;
-        
-        // shrink the bounds of your view to compensate for the offset
-        viewBounds.size.height = viewBounds.size.height + (topBarOffset * -1);
-        self.view.bounds = viewBounds;
+- (void) viewDidLayoutSubviews
+{
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 7)
+    {
+        self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0);
     }
 }
 
@@ -57,7 +51,6 @@
     self.navigationController.navigationBarHidden = NO;
     self.navigationController.navigationBar.translucent = NO;
     
-
     //распознаватель жестов:
     
     SWRevealViewController *revealController = self.revealViewController;
